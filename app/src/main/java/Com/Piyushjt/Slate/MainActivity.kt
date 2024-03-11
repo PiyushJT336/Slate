0b3ee703ac9f383615f264eab1d312d1871db010
Piyush_jt__ <piyushthummar396@gmail.com>
2024-03-11 16:20:41 +0530

    * added logo  * added splash screen  * removed action bar

--- app/src/main/java/Com/Piyushjt/Slate/MainActivity.kt
+++ app/src/main/java/Com/Piyushjt/Slate/MainActivity.kt
@@ -1,17 +1,20 @@
-package Com.Piyushjt.Slate
+package com.Piyushjt.Slate
 
+import Com.Piyushjt.Slate.R
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        Thread.sleep(1000)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        actionBar?.hide()
     }
 }
